fee9abc5c19d56716d2639eecc7b07bda4c5182d
equester <kvishwakarma@quadratyx.com>
2018-04-30 19:19:28 +0530

    Add hindi models

--- src/main/java/Assistant.java
+++ src/main/java/Assistant.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
 
 /**
- * TODO
+ * Work well for US accent however the accuracy for Indian English is not good.
  * 
  * @author Krishna kumar vishwakarma
  * @date 30-Apr-2018
@@ -11,9 +17,25 @@ import edu.cmu.sphinx.api.SpeechResult;
 public class Assistant {
 	public static void main(String[] args) throws Exception {
 		Configuration configuration = new Configuration();
-		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+		Properties pro = new Properties();
+		String CONFPATH = "src/main/resources/application.properties";
+		File file = new File(CONFPATH);
+		String fileName = file.getAbsolutePath();
+		try {
+			pro.load(new FileInputStream(fileName));
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+		String acoustivModelPath = pro.getProperty("hindiAcousticModelPath");
+		String defaultDictionaryPath = pro.getProperty("hindiDictionaryPath");
+		String defaultLanguageModelPath = pro.getProperty("hindiLanguageModelPath");
+		
+		configuration.setAcousticModelPath(acoustivModelPath);
+		configuration.setDictionaryPath(defaultDictionaryPath);
+		configuration.setLanguageModelPath(defaultLanguageModelPath);
 		configuration.setUseGrammar(false);
 
 		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
